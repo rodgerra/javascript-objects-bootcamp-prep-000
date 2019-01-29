7541e2b1783214c7cf94ab9269b4cf7cbef12a79
Ranae Rodgers <ranae.rodgers@icloud.com>
2019-01-29 22:15:24 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,6 +4,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
     return playlist;
 }
 function removeFromPlaylist(playlist, artistName, songTitle) {
-  delete playlist["Slowdive"];
+  delete playlist.Slowdive;
   return playlist;
 }
